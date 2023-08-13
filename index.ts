c5407e05f73cc7a4fc49e47209fe8f8c2513cf90
Akshat Rawat <akshat010501@gmail.com>
2023-08-14 02:58:28 +0530

    refactor: replace TypeORM Columns with DateColumns

--- index.ts
+++ index.ts
@@ -1,26 +1,30 @@
 import express, { Express, Request, Response } from "express";
+import { createContactController } from "./src/controllers/ContactController";
 import { createConnection } from "typeorm";
 
 const app: Express = express();
+app.use(express.json());
 const port: number = 3000;
 
 createConnection({
-    type: "sqlite",
-    database: "src/db/database.sqlite",
-    synchronize: true,
-    logging: true,
-    entities: ["src/db/entity/**/*.ts"],
+  type: "sqlite",
+  database: "src/db/database.sqlite",
+  synchronize: true,
+  logging: true,
+  entities: ["src/db/entity/**/*.ts"],
+})
+  .then(() => {
+    console.log("Connected to the database");
   })
-    .then(() => {
-      console.log("Connected to the database");
-    })
-    .catch((error) => {
-      console.error("Database connection error:", error);
-    });
+  .catch((error) => {
+    console.error("Database connection error:", error);
+  });
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Server initiated - beep boop");
+  res.send("Make a post request to /identity");
 });
+
+app.post("/identity", createContactController);
 
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
--- src/db/entity/Contact.ts
+++ src/db/entity/Contact.ts
@@ -1,4 +1,12 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
 import { LinkPrecedenceEnum } from "../../utils/helper";
 
 @Entity()
@@ -8,7 +16,7 @@ export default class Contact extends BaseEntity {
   @Column({ nullable: true }) email: string;
   @Column({ nullable: true }) linkedId: number;
   @Column({ enum: LinkPrecedenceEnum }) linkPrecedence: string;
-  @Column() createdAt: Date;
-  @Column() updatedAt: Date;
-  @Column({ nullable: true }) deletedAt: Date;
+  @CreateDateColumn() createdAt: Date;
+  @UpdateDateColumn() updatedAt: Date;
+  @DeleteDateColumn({ nullable: true }) deletedAt: Date;
 }
